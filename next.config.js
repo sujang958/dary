c3a691962b79c95a499ffe828ced26e2ef5e28ed
sujang958 <sujangdev@gmail.com>
2023-12-04 23:06:55 +0900

    feat: add Million

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const million = require("million/compiler")
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true }
+
+const millionConfig = {
+  auto: true,
+  // if you're using RSC:
+  // auto: { rsc: true },
+}
+
+module.exports = million.next(nextConfig, millionConfig)
